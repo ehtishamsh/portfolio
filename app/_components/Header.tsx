eaa722f82ec10496befd4700e4ac4aad22bd65d3
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2023-12-31 10:07:27 +0500

    header

---
+++ app/_components/Header.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+function Header() {
+  const path = usePathname();
+  return (
+    <header className="bg-transparent flex justify-center items-center py-6 px-11">
+      <nav className="border border-slate-800 bg-transparent backdrop-blur-sm rounded-3xl text-gray-800 flex items-center w-3/5">
+        <Link
+          href={"/"}
+          className={`bg-transparent w-1/3 transition-all duration-300 rounded-3xl text-center p-2 ${
+            path === "/"
+              ? "text-gray-100 bg-gray-800 hover:bg-gray-700"
+              : "text-gray-800"
+          }`}
+        >
+          Home
+        </Link>
+        <Link
+          href={"#about"}
+          className={`bg-transparent w-1/3 transition-all duration-300 rounded-3xl text-center p-2 text-gray-800`}
+        >
+          About
+        </Link>
+        <Link
+          href={"#experience"}
+          className={`bg-transparent w-1/3 transition-all duration-300 rounded-3xl text-center p-2 text-gray-800`}
+        >
+          Experience
+        </Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ app/_components/HeroSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HeroSection() {
+  return (
+    <div className="h-[calc(100vh-80px)] w-full flex justify-center items-center p-7">
+      <div className="w-full h-full hero"></div>
+    </div>
+  );
+}
+
+export default HeroSection;
