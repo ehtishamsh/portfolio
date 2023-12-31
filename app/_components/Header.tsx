644f69f4a11fcfd3f890b2f9c5cfd9039094cba9
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2023-12-31 10:25:34 +0500

    some changes

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -6,14 +6,14 @@ import { usePathname } from "next/navigation";
 function Header() {
   const path = usePathname();
   return (
-    <header className="bg-transparent flex justify-center items-center py-6 px-11">
+    <header className="bg-transparent flex justify-center items-center py-6 px-11 fixed top-0 right-0 left-0">
       <nav className="border border-slate-800 bg-transparent backdrop-blur-sm rounded-3xl text-gray-800 flex items-center w-3/5">
         <Link
           href={"/"}
-          className={`bg-transparent w-1/3 transition-all duration-300 rounded-3xl text-center p-2 ${
+          className={`w-1/3 transition-all duration-300 rounded-3xl text-center p-2 ${
             path === "/"
               ? "text-gray-100 bg-gray-800 hover:bg-gray-700"
-              : "text-gray-800"
+              : "text-gray-800 bg-transparent "
           }`}
         >
           Home
--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function HeroSection() {
   return (
-    <div className="h-[calc(100vh-80px)] w-full flex justify-center items-center p-7">
-      <div className="w-full h-full hero"></div>
+    <div className="h-[calc(100vh-80px)] w-full flex justify-center items-center p-7 mt-10">
+      <div className="w-full h-full hero rounded-3xl"></div>
     </div>
   );
 }
