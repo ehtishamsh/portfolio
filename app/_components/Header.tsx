f73431def2b6f4cea82e980d1feb43359f65ced3
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-01-02 23:43:39 +0500

    improve

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -6,27 +6,27 @@ import { usePathname } from "next/navigation";
 function Header() {
   const path = usePathname();
   return (
-    <header className="bg-transparent flex justify-center items-center py-6 px-11 fixed top-0 right-0 left-0">
-      <nav className="border border-slate-800 bg-transparent backdrop-blur-sm rounded-3xl text-gray-800 flex items-center w-3/5">
+    <header className="bg-transparent flex justify-center items-center py-6 px-11 fixed top-0 right-0 left-0 z-50">
+      <nav className="border border-gray-950 bg-transparent backdrop-blur-sm rounded-3xl text-gray-950 flex items-center w-3/5 transition-all duration-300">
         <Link
           href={"/"}
           className={`w-1/3 transition-all duration-300 rounded-3xl text-center p-2 ${
             path === "/"
-              ? "text-gray-100 bg-gray-800 hover:bg-gray-700"
-              : "text-gray-800 bg-transparent "
+              ? "text-gray-100 bg-gray-950 hover:bg-gray-900"
+              : "text-gray-950 bg-transparent "
           }`}
         >
           Home
         </Link>
         <Link
           href={"#about"}
-          className={`bg-transparent w-1/3 transition-all duration-300 rounded-3xl text-center p-2 text-gray-800`}
+          className={`bg-transparent w-1/3 transition-all duration-300 rounded-3xl text-center p-2 text-gray-950`}
         >
           About
         </Link>
         <Link
           href={"#experience"}
-          className={`bg-transparent w-1/3 transition-all duration-300 rounded-3xl text-center p-2 text-gray-800`}
+          className={`bg-transparent w-1/3 transition-all duration-300 rounded-3xl text-center p-2 text-gray-950`}
         >
           Experience
         </Link>
--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -1,9 +1,19 @@
+"use client";
 import React from "react";
-
+import blob from "../_components/_animation/blob.json";
+import Lottie from "lottie-react";
 function HeroSection() {
   return (
-    <div className="h-[calc(100vh-80px)] w-full flex justify-center items-center p-7 mt-10">
-      <div className="w-full h-full hero rounded-3xl"></div>
+    <div className="h-[calc(100vh-80px)] w-full flex justify-center items-center p-7 mt-16">
+      <div className="w-full h-full relative">
+        <div className="w-full h-full flex flex-col justify-center items-center text-white">
+          <h1 className="text-5xl font-bold mb-5 z-30">Hi, I am Ehtisham.</h1>
+          <p className="text-2xl  z-30 ">Full Stack Web Developer</p>
+        </div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[55%] overflow-hidden -z-10">
+          <Lottie animationData={blob} loop={true} style={{ height: "50%" }} />
+        </div>
+      </div>
     </div>
   );
 }
