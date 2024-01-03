39227c05fbf15bd00090820921a8e8ed31bff95c
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-01-03 08:57:52 +0500

    completed hero section

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -7,12 +7,12 @@ function Header() {
   const path = usePathname();
   return (
     <header className="bg-transparent flex justify-center items-center py-6 px-11 fixed top-0 right-0 left-0 z-50">
-      <nav className="border border-gray-950 bg-transparent backdrop-blur-sm rounded-3xl text-gray-950 flex items-center w-3/5 transition-all duration-300">
+      <nav className="border border-white bg-transparent backdrop-blur-sm rounded-3xl text-gray-950 flex items-center w-3/5 transition-all duration-300">
         <Link
           href={"/"}
           className={`w-1/3 transition-all duration-300 rounded-3xl text-center p-2 ${
             path === "/"
-              ? "text-gray-100 bg-gray-950 hover:bg-gray-900"
+              ? "text-black bg-white hover:bg-gray-200"
               : "text-gray-950 bg-transparent "
           }`}
         >
@@ -20,13 +20,13 @@ function Header() {
         </Link>
         <Link
           href={"#about"}
-          className={`bg-transparent w-1/3 transition-all duration-300 rounded-3xl text-center p-2 text-gray-950`}
+          className={`bg-transparent w-1/3 transition-all duration-300 rounded-3xl text-center p-2 text-white hover:text-gray-400 `}
         >
           About
         </Link>
         <Link
           href={"#experience"}
-          className={`bg-transparent w-1/3 transition-all duration-300 rounded-3xl text-center p-2 text-gray-950`}
+          className={`bg-transparent w-1/3 transition-all duration-300 rounded-3xl  text-center p-2 text-white hover:text-gray-400 `}
         >
           Experience
         </Link>
--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -1,18 +1,58 @@
 "use client";
 import React from "react";
-import blob from "../_components/_animation/blob.json";
-import Lottie from "lottie-react";
+import {
+  FaFacebook,
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+  FaArrowRight,
+} from "react-icons/fa";
 function HeroSection() {
   return (
-    <div className="h-[calc(100vh-80px)] w-full flex justify-center items-center p-7 mt-16">
+    <div className="h-[calc(100vh-80px)] w-full flex justify-center items-center p-7 max-sm:p-3 mt-16">
       <div className="w-full h-full relative">
-        <div className="w-full h-full flex flex-col justify-center items-center text-white">
-          <h1 className="text-5xl font-bold mb-5 z-30">Hi, I am Ehtisham.</h1>
-          <p className="text-2xl  z-30 ">Full Stack Web Developer</p>
+        <div className="w-full h-full flex flex-col justify-center items-center text-black">
+          <h1 className="text-[5.5vw] max-sm:text-[7.5vw] mb-5 z-30">
+            Hi, I am <span className="font-bold">Ehtisham.</span>
+          </h1>
+          <p className="text-[2.5vw] max-sm:text-[4.5vw] font-bold z-30  m-0">
+            Full Stack Web{" "}
+            <span className="dev font-sans text-white">Developer</span>
+          </p>
+          <div className="flex gap-6 justify-between pt-11 max-xl:pt-5 max-lg:grid max-lg:grid-cols-4">
+            <a
+              href="#"
+              className="p-4 bg-white border-2 flex justify-center max-md:p-2 max-lg:col-span-4 items-center gap-3  border-black cursor-pointer hover:bg-black hover:text-white transition-all duration-300 text-black rounded-lg"
+            >
+              Contact me <FaArrowRight />
+            </a>
+            <a
+              href="#"
+              className="p-4 bg-white border-2 flex justify-center max-md:p-2 items-center border-black cursor-pointer hover:bg-black hover:text-white transition-all duration-300 text-black rounded-lg"
+            >
+              <FaFacebook className="z-30 text-2xl max-md:text-xl" />
+            </a>
+            <a
+              href="#"
+              className="p-4 bg-white border-2 flex justify-center max-md:p-2 items-center border-black cursor-pointer hover:bg-black hover:text-white transition-all duration-300 text-black rounded-lg"
+            >
+              <FaGithub className="z-30 text-2xl max-md:text-xl" />
+            </a>
+            <a
+              href="#"
+              className="p-4 bg-white border-2 flex justify-center max-md:p-2 items-center border-black cursor-pointer hover:bg-black hover:text-white transition-all duration-300 text-black rounded-lg"
+            >
+              <FaLinkedin className="z-30 text-2xl max-md:text-xl" />
+            </a>
+            <a
+              href="#"
+              className="p-4 bg-white border-2  border-black cursor-pointer max-md:p-2 justify-center items-center flex hover:bg-black hover:text-white transition-all duration-300 text-black rounded-lg"
+            >
+              <FaTwitter className="z-30 text-2xl max-md:text-xl" />
+            </a>
+          </div>
         </div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[55%] overflow-hidden -z-10">
-          <Lottie animationData={blob} loop={true} style={{ height: "50%" }} />
-        </div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 hero -translate-y-1/2 w-[65%] h-[90%] max-sm:h-[60%] max-md:w-full max-md:h-[85%] max-lg:w-[90%] max-lg:h-[85%]  bg-white -z-10"></div>
       </div>
     </div>
   );
