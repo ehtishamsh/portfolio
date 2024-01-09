46d3b0defc91733d5544d686ab13ec404c633761
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-01-09 09:39:25 +0500

    About me page

--- app/_components/AboutMe.tsx
+++ app/_components/AboutMe.tsx
@@ -1,7 +1,20 @@
+"use client";
 import React from "react";
+import { Reveal } from "./_animation/Reveal";
 
 function AboutMe() {
-  return <div>LKJ</div>;
+  return (
+    <div className="w-full h-screen p-7 flex flex-col  items-center bg-white text-black">
+      <Reveal delayTime={0.4}>
+        <h1 className="text-5xl mt-11">
+          About <span className="font-extrabold">Me</span>
+        </h1>
+      </Reveal>
+      <div className="w-full flex justify-between items-center gap-5">
+        <div className="w-1/2"></div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutMe;
--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         <HeroSection />
       </PageWrapper>
       <MySkills />
+      <AboutMe />
     </div>
   );
 }
