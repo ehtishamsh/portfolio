6bb27a56559f411d0d2b8ecd43728567ab2609c5
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-01-18 12:54:42 +0500

    Added some project details and types

--- app/_components/Projects.tsx
+++ app/_components/Projects.tsx
@@ -1,10 +1,38 @@
 import React from "react";
 
-const data = {
-  data: [
+interface dataType {
+  project_data: [
+    {
+      name: string;
+      id: number;
+      url: string;
+      img_url: string;
+      description: string;
+    }
+  ];
+}
+
+const data: dataType = {
+  project_data: [
     {
       name: "Electronics Gala",
       id: 1,
+      url: "",
+      img_url: "/Project-1.png",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium illo neque consequatur aliquid amet. Quibusdam vero, officia doloremque deleniti aliquam, tempora fuga vitae quisquam, consectetur dolores sit explicabo nihil iusto.",
+    },
+    {
+      name: "Electronics Gala",
+      id: 2,
+      url: "",
+      img_url: "/Project-1.png",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium illo neque consequatur aliquid amet. Quibusdam vero, officia doloremque deleniti aliquam, tempora fuga vitae quisquam, consectetur dolores sit explicabo nihil iusto.",
+    },
+    {
+      name: "Electronics Gala",
+      id: 3,
       url: "",
       img_url: "/Project-1.png",
       description:
