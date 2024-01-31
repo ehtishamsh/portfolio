3209f0947b02020296e2cb8ddcf707d126bcea2a
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-02-01 03:55:20 +0500

    removed some text

--- app/_components/utils/SingleProject.tsx
+++ app/_components/utils/SingleProject.tsx
@@ -9,7 +9,7 @@ function SingleProject() {
             src="https://plus.unsplash.com/premium_photo-1674289121415-ec50cba976b6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8"
             className="object-cover max-w-[580px] max-h-[420px]"
             alt=""
-          />{" "}
+          />
         </div>
         <div>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam non
