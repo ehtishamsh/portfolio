f5a7e9dd304e0a434a70fd6c45ceeb96bd2b8add
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-02-02 07:13:38 +0500

    change image paths

--- app/_components/utils/projectData.ts
+++ app/_components/utils/projectData.ts
@@ -4,16 +4,16 @@ export default {
       name: "Electronics Gala",
       id: 1,
       url: "https://github.com/ehtishamsh/Electronics-Gala",
-      img_url: "Project1.png",
+      img_url: "/Project1.png",
       description:
         "Electronic Gala is a user-friendly, responsive electronic ecommerce website that allows users to browse and purchase a wide range of electronic gadgets and accessories. This website is built using a combination of front-end and back-end technologies to ensure a smooth and secure shopping experience.",
-      language: ["SiPhp", "siMysql", "SiJavascript", "SiBootstrap"],
+      language: ["SiPhp", "SiMysql", "SiJavascript", "SiBootstrap"],
     },
     {
       name: "Filmbox",
       id: 2,
       url: "https://github.com/ehtishamsh/FIlmBox",
-      img_url: "Project2.png",
+      img_url: "/Project2.png",
       description:
         "Filmbox is a comprehensive movies and TV shows database website that allows you to explore a vast library, view detailed information, search using a powerful search bar, and watch embedded trailers. The site seamlessly integrates with the TMDB API to provide users with an immersive experience.",
       language: ["SiReact", "SiTailwindcss", "SiJavascript", "SiAxios"],
