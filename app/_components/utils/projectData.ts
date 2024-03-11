1ee6843cdf112a0a95dc1136f49b2d7fa941f31f
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-03-12 01:30:29 +0500

    added new project

--- app/_components/utils/projectData.ts
+++ app/_components/utils/projectData.ts
@@ -1,19 +1,34 @@
 export default {
   project_data: [
     {
-      name: "Electronics Gala",
+      name: "The Blog",
       id: 1,
-      url: "https://github.com/ehtishamsh/Electronics-Gala",
+      url: "https://github.com/ehtishamsh/theblog",
       img_url: "project1.png",
+      description:
+        "The Blog is a dynamic blogging platform built with Shadcn UI, TypeScript, Next.js 14, Prisma,Next-Auth, and PostgreSQL. Users can seamlessly read and publish blogs using a user-friendly interface. The editor is powered by Tiptap, providing a rich and intuitive writing experience.",
+      language: [
+        "SiTypescript",
+        "SiNextdotjs",
+        "SiPrisma",
+        "SiPostgresql",
+        "SiTailwindcss",
+      ],
+    },
+    {
+      name: "Electronics Gala",
+      id: 2,
+      url: "https://github.com/ehtishamsh/Electronics-Gala",
+      img_url: "project2.png",
       description:
         "Electronic Gala is a user-friendly, responsive electronic ecommerce website that allows users to browse and purchase a wide range of electronic gadgets and accessories. This website is built using a combination of front-end and back-end technologies to ensure a smooth and secure shopping experience.",
       language: ["SiPhp", "SiMysql", "SiJavascript", "SiBootstrap"],
     },
     {
       name: "Filmbox",
-      id: 2,
+      id: 3,
       url: "https://github.com/ehtishamsh/FIlmBox",
-      img_url: "project2.png",
+      img_url: "project3.png",
       description:
         "Filmbox is a comprehensive movies and TV shows database website that allows you to explore a vast library, view detailed information, search using a powerful search bar, and watch embedded trailers. The site seamlessly integrates with the TMDB API to provide users with an immersive experience.",
       language: ["SiReact", "SiTailwindcss", "SiJavascript", "SiAxios"],
