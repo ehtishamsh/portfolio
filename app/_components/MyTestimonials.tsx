2a7a8a4d7f423f7ced9dff0db20abea3ffca0f82
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-03-28 05:34:50 +0500

    changed testi image

--- app/_components/MyTestimonials.tsx
+++ app/_components/MyTestimonials.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { Reveal } from "./_animation/Reveal";
+import { PiQuotes } from "react-icons/pi";
 
 function MyTestimonials() {
   return (
@@ -15,21 +16,21 @@ function MyTestimonials() {
           <div className="grid grid-cols-3  items-center max-sm:grid-cols-1 justify-center gap-7">
             <div className=" justify-center items-center flex ">
               <div className="flex flex-col justify-center items-center gap-6 w-[90%] bg-white rounded-2xl px-14 py-12 ">
-                <div className="w-24 h-24 rounded-full overflow-hidden border border-gray-400">
+                <div className="w-24 h-24  relative">
                   <img
                     src="duck.jpg"
-                    className=" w-full h-full object-cover border border-gray-950"
+                    className=" w-full h-full  rounded-full object-cover border border-gray-950"
                     alt=""
                   />
+                  <PiQuotes className="text-2xl absolute -bottom-1 z-20 right-0" />
                 </div>
-                <p className="text-base text-gray-600 text-center">
-                  I recently had to jump on 10+ different calls across eight
-                  different countries to find the right owner.
+                <p className="relative text-base text-gray-600 text-center">
+                  {
+                    " This was an elite experience  for all the quality you would look for in a timely professional and exceptional artist. I highly recommend! Ilook forward to working together in the future."
+                  }
                 </p>
                 <hr className="p-[1.5px] bg-black w-[50%] rounded-md" />
-                <h5 className="text-base font-semibold text-gray-600">
-                  Ehtisham Shah
-                </h5>
+                <h5 className="text-base font-semibold text-gray-600">Ray</h5>
                 <h5 className="text-sm font-semibold text-gray-600">
                   Upwork Client
                 </h5>
@@ -50,7 +51,7 @@ function MyTestimonials() {
                 </p>
                 <hr className="p-[1.5px] bg-black w-[50%] rounded-md" />
                 <h5 className="text-base font-semibold text-gray-600">
-                  Ehtisham Shah
+                  Afn Alen
                 </h5>
 
                 <h5 className="text-sm font-semibold text-gray-600">
