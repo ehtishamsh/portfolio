3ec8c0a8b8c05702d6885d8bd5d67223c5f47232
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-03-30 07:03:00 +0500

    completed contact me section

--- app/_components/ContactMe.tsx
+++ app/_components/ContactMe.tsx
@@ -4,12 +4,46 @@ import { Reveal } from "./_animation/Reveal";
 
 function ContactMe() {
   return (
-    <div className="w-full  mt-20 px-10 py-8  max-sm:px-4 max-sm:py-8 max-xxxxs:py-3 max-xxxxs:mt-10  relative ">
-      <Reveal delayTime={0.8} width="100%">
-        <h1 className="text-5xl max-lg:text-3xl mb-8 max-lg:mb-4 max-xxxxs:text-center z-30 text-white text-center">
-          Contact <span className="font-extrabold">Me</span>
-        </h1>
-      </Reveal>
+    <div className="bg-white py-14 mt-20 max-sm:py-8 max-sm:mt-12">
+      <div className=" max-w-7xl mx-auto  px-10 py-8  max-sm:px-4 max-sm:py-8 max-xxxxs:py-3   relative  ">
+        <div className="grid grid-cols-2 gap-28 max-sm:gap-12 max-md:grid-cols-1 px-5">
+          <div>
+            <form
+              action=""
+              className="flex justify-center items-center gap-7 flex-col"
+            >
+              <input
+                type="text"
+                className="border-2 border-gray-800 rounded p-2 text-sm w-full "
+                placeholder="Name"
+              />
+              <input
+                type="text"
+                className="border-2 border-gray-800 rounded p-2 text-sm w-full "
+                placeholder="Email"
+              />
+              <textarea
+                rows={5}
+                className="border-2 border-gray-800 rounded p-2 text-sm w-full "
+                placeholder="How can I help you?"
+              />
+              <button className="border-2 border-gray-800 rounded-md p-2 text-sm w-full  bg-black text-white">
+                Get In Touch
+              </button>
+            </form>
+          </div>
+          <div className="flex justify-center w-full flex-col max-sm:-order-1">
+            <h1 className="text-5xl max-sm:text-4xl font-bold mb-6">
+              Let's <span className="outline-title">talk</span> for <br />
+              Something special
+            </h1>
+            <p className="text-base text-gray-600 max-sm:text-sm">
+              I seek to push the limits of creativity to create high-engaging,
+              user-friendly, and memorable interactive experiences.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
